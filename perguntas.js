37b040231c48b06f930a5ddeed5f44f79d32cd64
Ana clara Macedo S. <00001130288924sp@al.educacao.sp.gov.br>
2024-11-04 10:54:20 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'literatura',
+    'qual o livro mais conhecido de machado de assis?',
+    'o livro mais conhecudo e memorias postumas de bras cubas'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da italia?',
+    'A capital da italia é roma'
+)
+
+criaCartao(
+    'Programação',
+    'O que é  algoritmo?',
+    'conjunto de instruçoes que visam resolver um problema ou execultadar uma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz desculpa em Inglês?',
+    'desculpa em ingles é sorry (sorry)'
+)
